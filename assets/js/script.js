8bb03ed33d02384f5c9d2f37a7e47650aa0fe945
Bemourn <70115233+koenib@users.noreply.github.com>
2021-12-08 21:35:23 -0700

    resolve merge conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // username options
 // verbs and nouns are mutally exclusive
 // common and uncommon words are mutually exclusive
@@ -8,20 +7,6 @@ const maxWordLengthSlider = $('#word-max'); // id="word-max"
 const commonWordInput = $('#common'); // id="common"
 const uncommonWordInput = $('#uncommon'); // id="uncommon"
 const nameLengthEl = $('#name-length-value'); // id="name-length"
-=======
-// JavaScript logic
-
-// declare global vars
-// username options
-const verbInput = $('#verbs'); // id="verbs"
-const nounInput = $('#nouns'); // id="nouns"
-const maxWordLength = $('#word-max'); // id="word-max"
-const commonWordInput = $('#common'); // id="common"
-const uncommonWordInput = $('#uncommon'); // id="uncommon"
-
-// verbs and nouns are mutally exclusive
-// common and uncommon words are mutually exclusive
->>>>>>> 9a1a7eb (added button features)
 
 // password options
 const lowerInput = $('#lowercase'); // id="lowercase"
@@ -29,7 +14,6 @@ const upperInput = $('#uppercase'); // id="uppercase"
 const numInput = $('#numbers'); // id="numbers"
 const specialInput = $('#special'); // id="special"
 const passLengthSlider = $('#passLength'); // id="passLength"
-<<<<<<< HEAD
 const passLengthEl = $('#pass-length-value'); // id="passLength"
 
 // Initialize the necessary checkboxes so they are checked when the page loads.
@@ -45,38 +29,23 @@ numInput.attr('checked', true);
 specialInput.attr('checked', true);
 
 // Define UTF codes representing the set of possible password characters per setting
-=======
-
-// Initialize necessary checkboxes so they are checked when the page loads.
-// default options are loaded for each, usernames and passwords
-verbInput.checked = true;
-commonWordInput.checked = true;
-
-lowerInput.checked = true;
-upperInput.checked = true;
-numInput.checked = true;
-specialInput.checked = true;
-
-// Define UTF codes for allowed password characters
->>>>>>> 9a1a7eb (added button features)
 const lowerChars = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
 const upperChars = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
 const numChars = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
 const specialChars = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126];
 
-<<<<<<< HEAD
 // global arrays, objects, variables
-=======
->>>>>>> 9a1a7eb (added button features)
 let possibleChars = [];
 let userPass = {};
 let nums = [];
+
+// buttons
+// $("#btn-save")
 
 // Initialize API parameters
 let partOfSpeech = 'verb';
 let minimumWordFrequency = '1000';
 
-<<<<<<< HEAD
 // Set the input (checkbox) values so they remain mutally exclusive
 $('input:checkbox').change(
     function () {
@@ -100,6 +69,7 @@ $('input:checkbox').change(
         } else if ($(this).attr('id') === 'uncommon' && !commonWordInput.is(':checked')) {
             $(this).prop('checked', true);
         }
+
     });
 
 passLengthSlider.on('input', () => {
@@ -111,8 +81,6 @@ maxWordLengthSlider.on('input', () => {
 });
 
 // monkey-proof generation options, reject if no option is chosen
-=======
->>>>>>> 9a1a7eb (added button features)
 const validatePassInput = () => {
     // I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
     // my input should be validated and at least one character type should be selected
@@ -124,7 +92,6 @@ const validatePassInput = () => {
     }
 }
 
-<<<<<<< HEAD
 const initPassSettings = () => {
 
     if (lowerInput.checked) {
@@ -142,81 +109,48 @@ const generatePassword = () => {
 
     // Choose password length 8-128 characters
     let passLength = passLengthSlider.value;
-=======
-const generatePassword = () => {
+    const generatePassword = () => {
 
-    // Choose password length 8-128 characters
-    let passLength = 64; // passLengthSlider.value;
->>>>>>> 9a1a7eb (added button features)
-    let passText = $('#pw-text'); // id="pw-text"
-    let newChar = '';
-    let nextChar = '';
-    let myString = '';
-<<<<<<< HEAD
+        // Choose password length 8-128 characters
+        let passLength = 64; // passLengthSlider.value;
+        let passText = $('#pw-text'); // id="pw-text"
+        let newChar = '';
+        let nextChar = '';
+        let myString = '';
 
-=======
-    
->>>>>>> 9a1a7eb (added button features)
-    passText.innerHTML = '';
-    possibleChars = [];
-    nums = [];
+        passText.innerHTML = '';
+        possibleChars = [];
+        nums = [];
 
-    initPassSettings();
+        initPassSettings();
 
-    let queryURL = `https://www.random.org/decimal-fractions/?num=${passLength}&dec=20&col=1&format=plain&rnd=new`;
+        let queryURL = `https://www.random.org/decimal-fractions/?num=${passLength}&dec=20&col=1&format=plain&rnd=new`;
 
-    fetch(queryURL).then(response => {
-        response.text().then(data => ({
-            data: data,
-            status: response.status
-        })).then(res => {
-            if (res.status === 200) {
-                console.log(`Status: ${res.status} OK`);
-                console.log(res.data);
-                nums = res.data.split('\n');
-                console.log(nums);
-<<<<<<< HEAD
+        fetch(queryURL).then(response => {
+            response.text().then(data => ({
+                data: data,
+                status: response.status
+            })).then(res => {
+                if (res.status === 200) {
+                    console.log(`Status: ${res.status} OK`);
+                    console.log(res.data);
+                    nums = res.data.split('\n');
+                    console.log(nums);
 
-                // a password is generated that matches the selected criteria
-                for (let i = 0; i < passLength; i++) {
-                    nextChar = Math.floor(nums[i] * (possibleChars.length - 1));
-                    newChar = String.fromCharCode(possibleChars[nextChar]);
-                    myString += newChar;
+                    // a password is generated that matches the selected criteria
+                    for (let i = 0; i < passLength; i++) {
+                        nextChar = Math.floor(nums[i] * (possibleChars.length - 1));
+                        newChar = String.fromCharCode(possibleChars[nextChar]);
+                        myString += newChar;
+                    }
+
+                    console.log(myString);
                 }
+            })
+        });
 
-                console.log(myString);
-            }
-        })
-    });
-
-=======
-
-                // a password is generated that matches the selected criteria
-                for (let i = 0; i < passLength; i++) {
-                    nextChar = Math.floor(nums[i] * (possibleChars.length - 1));
-                    newChar = String.fromCharCode(possibleChars[nextChar]);
-                    myString += newChar;
-                }
-
-                console.log(myString);
-            }
-        })
-    });
-
-}
-
-const initPassSettings = () => {
-
-    if (lowerInput.checked) {
-        possibleChars = possibleChars.concat(lowerChars);
-    } else if (upperInput.checked) {
-        possibleChars = possibleChars.concat(upperChars);
-    } else if (numInput.checked) {
-        possibleChars = possibleChars.concat(numInput);
-    } else if (specialInput.checked) {
-        possibleChars = possibleChars.concat(specialChars);
     }
->>>>>>> 9a1a7eb (added button features)
+
 }
 
 // and displayed on the page
@@ -248,7 +182,6 @@ const initPassSettings = () => {
 
 // when all prompts are answered
 
-<<<<<<< HEAD
 const initUsernameSettings = () => {
     // check the options selected by the user to use for generating the username
     // I can confirm whether to include verbs, nouns, and the dictionary frequency of words (wordnik option)
@@ -265,18 +198,12 @@ const initUsernameSettings = () => {
     }
 }
 
-=======
->>>>>>> 9a1a7eb (added button features)
 const generateUsername = () => {
 
     // array of objects of words, only retrieving limit = 2 words for now
     queryURL = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=${partOfSpeech}&minCorpusCount=${minimumWordFrequency}&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=15&limit=2&api_key=`
 
     initUsernameSettings();
-<<<<<<< HEAD
-=======
-    
->>>>>>> 9a1a7eb (added button features)
 
     fetch(queryURL).then(response =>
         response.json().then(data => ({
@@ -299,56 +226,30 @@ const generateUsername = () => {
             }
         }));
 }
-<<<<<<< HEAD
 
 // I am able to press the button and generate both at the same time
 // generateUsername();
-// generatePassword();
-
-function saveNamePass() {
-
-=======
-
-const initUsernameSettings = () => {
-    // check the options selected by the user to use for generating the username
-    // I can confirm whether to include verbs, nouns, and the dictionary frequency of words (wordnik option)
-    if (verbInput.checked) {
-        partOfSpeech = 'verb';
-    } else {
-        partOfSpeech = 'noun';
-    }
-
-    if (commonWordInput.checked) {
-        minimumWordFrequency = '1000';
-    } else {
-        minimumWordFrequency = '100';
-    }
-}
-
-// I am able to press the button and generate both at the same time
-generateUsername();
 generatePassword();
 
 function saveNamePass() {
-    
->>>>>>> 9a1a7eb (added button features)
+
     let namePass = JSON.parse(localStorage.getItem(`namePass`) || "[]");
     namePass.push(userPass);
     localStorage.setItem(`namePass`, JSON.stringify(namePass));
 
 }
 
-saveButton.on('click', function () {
-    let userName = $('textarea[name=Username]').val();
-    let password = $('textarea[name=Password]').val();
-    userPass = {
-        'user': userName,
-        'pass': password,
-    };
-    saveNamePass();
-});
+// save button logic
 
-
+// saveButton.on('click', function () {
+//     let userName = $('textarea[name=Username]').val();
+//     let password = $('textarea[name=Password]').val();
+//     userPass = {
+//         'user': userName,
+//         'pass': password,
+//     };
+//     saveNamePass();
+// });
 
 // I am able to press the button and generate both at the same time
 // I have the option to save my username + password combo for later
