84d2e24878c365d389a2e9a1e9a9231ba19eece9
Bemourn <70115233+koenib@users.noreply.github.com>
2021-12-09 16:58:18 -0700

    finalize logic, buttons flow

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -6,19 +6,28 @@ const clearBtn = $('#clear-btn');
 const namePass = JSON.parse(localStorage.getItem(`namePass`));
 
 // dynamically add all saved information from storage + the new highscore to the screen
-for (var i = 0; i < namePass.length; i++) {
-    var namePassItem = $('<li>')
-        .text(`${i+1}. ${namePass[i].username} - ${namePass[i].password}`)
-        .addClass('btn btn-dark btn-rounded');
+for (let i = 0; i < namePass.length; i++) {
+    let namePassItem = $('<li>')
+        .text(`${i+1}. User: \u00A0 ${namePass[i].user}`)
+        .addClass('btn btn-black btn-rounded my-1')
+        .attr('style', "font-size: 1.05em; background: linear-gradient(to bottom right, #000099 5%, #66ccff 129%");
+    let passNameItem = $('<li>')
+        .text(`${namePass[i].pass}`)
+        .addClass('btn btn-rounded text-white my-1')
+        .attr('style', "background: linear-gradient(to bottom right, #000099 5%, #66ccff 129%");
     namePassEl.append(namePassItem);
+    namePassEl.append($('<br />'));
+    namePassEl.append(passNameItem);
+    namePassEl.append($('<br />'));
 }
 
-// clear button
-clearBtn.on('click', function () {
-    $('li').each(function () {
+// when the clear everything button is pressed, all saved data is deleted and the page is refreshed
+clearBtn.on('click', () => {
+    $('li').each(() => {
         $(this).remove();
     });
     // clear localStorage
     localStorage.setItem(`namePass`, "[]");
-});
 
+    window.location.href = 'saved.html';
+});
